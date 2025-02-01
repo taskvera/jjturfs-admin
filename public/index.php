bba9d9b16cff31d89dd1c406242650bf994688dd
Anthony Desktop <anthony@dfrgrp.com>
2025-02-01 16:26:22 -0500

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+/public/index.php
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>JJ Turfs Index Page</title>
+  <!-- Tailwind CSS -->
+  <script src="https://cdn.tailwindcss.com"></script>
+  <!-- Font Awesome -->
+  <script 
+    src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"
+    crossorigin="anonymous">
+  </script>
+</head>
+<body>
+    
+</body>
+</html>
